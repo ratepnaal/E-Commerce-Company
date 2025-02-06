83abd091aed1ae4d51a52e8c2af96438d7450abb
ratepnaal <163038280+ratepnaal@users.noreply.github.com>
2025-02-06 22:28:07 +0300

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -17,7 +17,7 @@ import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 import api from "../api";
 const Login = ({ darkMode, setDarkMode }) => {
-  const username = localStorage.getItem('username')
+  const username = localStorage.getItem('username') || '';
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -25,64 +25,82 @@ const Login = ({ darkMode, setDarkMode }) => {
   const Images = [Photo1, Photo2, Photo3, Photo4, Photo5, Photo6];
   const [showModalError, setShowModalError] = useState(false);
   const [Invailed, setInvailed] = useState("");
-  const [showSuccess , setShowSuccess] = useState(false);
-  const handleNotification = () => {
-    // طلب الإذن من المستخدم
-    Notification.requestPermission().then(permission => {
-      if (permission === "granted") {
-        addNotification({
-          title: 'Welcome Back 🎉',
-          message: `Hello ${username} ! `,
-          duration: 4000,
-          native: true,
-          icon: Logo,
-          onClick: () => console.log("Push Notification!")
-        });
-      } else {
-        console.log("Notification permission denied");
-      }
-    });
-  };
-  // Images
+  const [showSuccess, setShowSuccess] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  //    Notification.requestPermission().then(permission => {
+  //     if (permission === "granted") {
+  //       addNotification({
+  //         title: 'Welcome Back 🎉',
+  //         message: `Hello ${username} ! `,
+  //         duration: 4000,
+  //         native: true,
+  //         icon: Logo,
+  //         onClick: () => console.log("Push Notification!")
+  //       });
+  //     } else {
+  //       console.log("Notification permission denied");
+  //     }
+  //   });
+  // };
+
+  // تغيير الصور كل 10 دقائق
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentImage((prevImage) => (prevImage + 1) % Images.length);
-    }, 10000); //change after 10 minutes
+    }, 10000); 
     return () => clearInterval(interval);
-  }, [Images.length]);
+  }, [Images]);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    // ارسال البيانات للداتابيز عندما كان السيرفر يعمل 
-    // try {
-    //   const credentials = { email, password };
-    //   const response = await api.post('/login', credentials);
-    //   console.log("login successfully " , response)
+    setIsLoading(true);
 
-    //   const { accessToken, refreshToken } = response.data;
-    //   localStorage.setItem('accessToken', accessToken);
-    //   localStorage.setItem('refreshToken', refreshToken);
-    //   localStorage.setItem('email',email); 
-    //   // عرض نافذة النجاح
+    // تعطيل طلب API ووضعه في تعليق
+    /*
+    try {
+      const credentials = { email, password };
+      const response = await api.post('/login', credentials);
+      console.log("تم تسجيل الدخول بنجاح", response);
+
+      const { accessToken, refreshToken } = response.data;
+      localStorage.setItem('accessToken', accessToken);
+      localStorage.setItem('refreshToken', refreshToken);
+      localStorage.setItem('email', email);
+
       setShowSuccess(true);
-      handleNotification();   
-      // تأخير التنقل حتى تظهر نافذة النجاح
       setTimeout(() => {
-        setShowSuccess(false);     // إخفاء النافذة بعد التأخير
+        setShowSuccess(false);
         navigate("/profile");
-      }, 4000);                 // تأخير لمدة ثانيتين
-    // } catch (error) {
-    //   setInvailed(t("error-message-login-failed"));
-    //   setShowModalError(true);
-    // }
+      }, 4000);
+    } catch (error) {
+      setInvailed(t("error-message-login-failed"));
+      setShowModalError(true);
+    } finally {
+      setIsLoading(false);
+    }
+    */
+
+    // تسجيل الدخول الديناميكي باستخدام معلومات وهمية
+    if (email && password) {
+      setShowSuccess(true);
+      setTimeout(() => {
+        setShowSuccess(false);
+        navigate("/profile"); // الانتقال إلى صفحة البروفايل
+      }, 4000);
+    } else {
+      setInvailed(t("error-message-login-failed"));
+      setShowModalError(true);
+    }
+    setIsLoading(false);
   };
-  
+
   const { t } = useTranslation();
 
   return (
-    <div className=" h-screen w-screen flex flex-col flex-grow ">
-
-      {/* Header*/}
-      <header className="w-full h-[50px] bg-[#1c5268] "></header>
+    <div className="h-screen w-screen flex flex-col flex-grow">
+      {/* Header */}
+      <header className="w-full h-[50px] bg-[#1c5268]"></header>
       <div className="flex flex-grow">
         {/* Left Section */}
         <div className="w-full lg:w-1/2 flex flex-col justify-center items-center p-8">
@@ -98,7 +116,7 @@ const Login = ({ darkMode, setDarkMode }) => {
             autoComplete="on"
             className="w-3/4 flex flex-col items-center justify-center"
           >
-            {/* Email*/}
+            {/* Email */}
             <div className="relative mb-6">
               <label className="text-[10px] block mb-2">
                 {t("email_label")}
@@ -108,10 +126,9 @@ const Login = ({ darkMode, setDarkMode }) => {
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 placeholder="EXAMPLE@GMAIL.COM"
-                className={`h-6 pl-12 py-3 text-[10px] 
-                 bg-gray-200 border border-neutral-500 shadow-xl rounded-md ${
-                   darkMode ? `text-black` : ``
-                 }`}
+                className={`h-6 pl-12 py-3 text-[10px] bg-gray-200 border border-neutral-500 shadow-xl rounded-md ${
+                  darkMode ? `text-black` : ``
+                }`}
                 required
                 autoComplete="email"
               />
@@ -120,7 +137,7 @@ const Login = ({ darkMode, setDarkMode }) => {
               </span>
             </div>
 
-            {/* Password*/}
+            {/* Password */}
             <div className="relative mb-6">
               <label className="text-[10px] block mb-2">
                 {t("password_label")}
@@ -130,34 +147,28 @@ const Login = ({ darkMode, setDarkMode }) => {
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 placeholder="**********"
-                className={`h-6 pl-12 py-3 text-[10px] 
-                  bg-gray-200 border border-neutral-500 shadow-xl rounded-md ${
-                    darkMode ? `text-black` : ``
-                  }`}
+                className={`h-6 pl-12 py-3 text-[10px] bg-gray-200 border border-neutral-500 shadow-xl rounded-md ${
+                  darkMode ? `text-black` : ``
+                }`}
                 required
                 autoComplete="current-password"
               />
               <span className="absolute top-7 left-3 flex items-center">
-                <img
-                  src={IconPassword}
-                  alt="Password Icon"
-                  className="h-4 w-5"
-                />
+                <img src={IconPassword} alt="Password Icon" className="h-4 w-5" />
               </span>
             </div>
-            {/* Error messsage */}
+
+            {/* Error message */}
             {Invailed && (
-              <div
-                className="flex items-center justify-center 
-               border border-red-400 text-red-700 mx-28 py-2 mt-2 rounded-md mb-4"
-              >
+              <div className="flex items-center justify-center border border-red-400 text-red-700 mx-28 py-2 mt-2 rounded-md mb-4">
                 <span>
-                  <img src={IconError} className="h-5 w-5 mr-2"></img>
+                  <img src={IconError} className="h-5 w-5 mr-2" alt="Error Icon" />
                 </span>
                 <span className="text-sm">{Invailed}</span>
               </div>
             )}
-            {/*Link & Button */}
+
+            {/* Link & Button */}
             <div className="flex flex-col items-center justify-center mb-6">
               <div className="text-center">
                 <h4 className="text-[11px] inline">{t("dont-have")}</h4>
@@ -178,20 +189,19 @@ const Login = ({ darkMode, setDarkMode }) => {
                 </Link>
               </div>
             </div>
+
             <button
               type="submit"
-              className="w-28 h-8 border border-neutral-500 shadow-xl bg-green-500 flex items-center justify-center
-               hover:bg-green-600 text-black font-semibold py-2 rounded-lg text-[11px]"
+              className="w-28 h-8 border border-neutral-500 shadow-xl bg-green-500 flex items-center justify-center hover:bg-green-600 text-black font-semibold py-2 rounded-lg text-[11px]"
+              disabled={isLoading}
             >
-              {t("login")}
+              {isLoading ? "جاري التحميل..." : t("login")}
             </button>
           </form>
         </div>
 
-        {/*Right Section */}
+        {/* Right Section */}
         <div className="hidden lg:block lg:w-1/2 h-full pl-20">
-          {" "}
-          {/**Hide this section in small screen */}
           <div
             className="w-full h-full bg-cover bg-center transition-all duration-1000"
             style={{
@@ -199,11 +209,11 @@ const Login = ({ darkMode, setDarkMode }) => {
               backgroundSize: "cover",
               backgroundPosition: "center",
             }}
-          >
-          </div>
+          ></div>
         </div>
       </div>
-      {/**ERROR window*/}
+
+      {/* Error window */}
       {showModalError && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
           <div
@@ -218,8 +228,7 @@ const Login = ({ darkMode, setDarkMode }) => {
               {t("title")}
             </h2>
             <p className="text-gray-600 mb-2 text-xs">{t("message")}</p>
-            <hr className="w-4/5 border-b-2  border-gray-400 mx-auto my-4" />
-
+            <hr className="w-4/5 border-b-2 border-gray-400 mx-auto my-4" />
             <button
               onClick={() => setShowModalError(false)}
               className="text-red-500 font-semibold hover:underline text-xs"
@@ -229,8 +238,9 @@ const Login = ({ darkMode, setDarkMode }) => {
           </div>
         </div>
       )}
-         {/**Success window*/}
-         {showSuccess && (
+
+      {/* Success window */}
+      {showSuccess && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
           <div
             className="bg-white p-4 rounded-2xl shadow-lg text-center fade-in"
@@ -244,8 +254,7 @@ const Login = ({ darkMode, setDarkMode }) => {
               {t("title-of-succsess-login")}
             </h2>
             <p className="text-gray-600 mb-2 text-xs">{t("message-of-success-login")}</p>
-            <hr className="w-4/5 border-b-2  border-gray-400 mx-auto my-4" />
-
+            <hr className="w-4/5 border-b-2 border-gray-400 mx-auto my-4" />
             <button
               onClick={() => setShowSuccess(false)}
               className="text-green-600 font-semibold hover:underline text-xs"
